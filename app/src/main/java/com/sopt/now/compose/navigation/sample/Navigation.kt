b5c3d4349098fe3bed8de97ae36ba3ea85923ab1
KwakEuiJin <yuijin0909@gmail.com>
2024-05-07 11:45:55 +0900

    Step 2: NavHost, NavController, NagGraph 추가

---
+++ app/src/main/java/com/sopt/now/compose/navigation/sample/Navigation.kt
@@ -0,0 +1,72 @@
+package com.sopt.now.compose.navigation.sample
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+        composable("detail") {
+            DetailScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "HOME")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            navController.navigate("detail")
+        }) {
+            Text(text = "Go to Detail")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "DETAIL")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            navController.navigate("home")
+        }) {
+            Text(text = "Go to Home")
+        }
+    }
+}
